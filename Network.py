243da698c4efa2f68ab1b542fb5d8a9c45039294
Mohsen Mozafari <73017441+MohsenMozaffary@users.noreply.github.com>
2023-03-18 03:38:55 -0400

    Add files via upload

---
+++ Network.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+
+
+
+class Net(nn.Module):
+    def __init__(self, dropout_rate=0.5):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 32, 3)
+        self.conv3 = nn.Conv2d(32, 64, 3)
+        self.global_pool = nn.AdaptiveAvgPool2d((1,1))
+        self.fc1 = nn.Linear(64, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 2)  
+        self.dropout_rate = dropout_rate
+        self.dropout = nn.Dropout(p=self.dropout_rate)
+
+    def forward(self, x):
+        x = self.pool(nn.functional.relu(self.conv1(x)))
+        x = self.dropout(x)
+        x = self.pool(nn.functional.relu(self.conv2(x)))
+        x = self.dropout(x)
+        x = self.pool(nn.functional.relu(self.conv3(x)))
+        x = self.dropout(x)
+        x = self.global_pool(x)
+        x = x.view(-1, 64)
+        x = nn.functional.relu(self.fc1(x.float()))
+        x = self.dropout(x)
+        x = nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
